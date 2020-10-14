f50878c77668d479f3e03abb58270bea5ad5d895
Mattia <m.dantonio81@gmail.com>
2020-10-14 15:47:55 +0200

    Debug code

--- projects/airods/backend/endpoints/airods.py
+++ projects/airods/backend/endpoints/airods.py
@@ -97,7 +97,7 @@ class AirodsInput(PartialSchema):
     # )
 
 
-class AirodsInputWithDownload(AirodsInput):
+class Download(PartialSchema):
     download = fields.Boolean(
         description="Allow download data or retrieve PID / URI of digital object",
         required=True,
@@ -148,8 +148,8 @@ class Airods(EndpointResource):
     labels = ["airods"]
 
     # @decorators.auth.require()
-    @decorators.use_kwargs(AirodsInputWithDownload, location="query")
-    # @decorators.use_kwargs(Download)
+    @decorators.use_kwargs(AirodsInput, location="query")
+    @decorators.use_kwargs(Download, location="query")
     @decorators.endpoint(
         path="/airods/data",
         summary="Get data from irods-b2safe via boundingbox-timewindow (epos ecosystem)",
