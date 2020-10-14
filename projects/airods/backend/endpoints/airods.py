edb6ad6bd00ef2065d7fd418ff65e8f20a3e1a7d
Mattia <m.dantonio81@gmail.com>
2020-10-14 15:23:06 +0200

    Bug fix

--- projects/airods/backend/endpoints/airods.py
+++ projects/airods/backend/endpoints/airods.py
@@ -370,8 +370,8 @@ class AirodsStage(EndpointResource):
 
     labels = ["airods"]
 
-    @decorators.use_kwargs(AirodsInput)
-    # @decorators.use_kwargs(StageInput)
+    # @decorators.use_kwargs(AirodsInput)
+    @decorators.use_kwargs(StageInput)
     @decorators.endpoint(
         path="/airods/stage",
         summary="Get data from irods-b2safe via boundingbox-timewindow and stage data to endpoint (epos ecosystem)",
